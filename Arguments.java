649581632ec1de4b809246de01c4560c3f4d309f
Juliet Ngoni <118878439+julietngoni22@users.noreply.github.com>
2024-04-23 00:27:41 +0200

    Add files via upload

---
+++ Arguments.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arguments;
+
+/**
+ *
+ * @author juliet
+ */
+public class Arguments {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("I can do maths");
+        System.out.println("6 + 4 = " + 6 + 4); //"(6+4 = " + 6 + 4) = 64
+        System.out.println("6 + 4 = " + (6 + 4)); //"(6+4 = " + (6 + 4)) = 10
+    }
+    
+}
