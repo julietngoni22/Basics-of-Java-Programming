c7f85b332b5a2871be34768bf5568daa87e30f5d
Juliet Ngoni <118878439+julietngoni22@users.noreply.github.com>
2024-04-23 00:26:12 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author juliet
+ */
+//classes
+//variables
+//Java can be used to create programs or applications such as Mobile apps, Desktop GUI App,Web-Based app.
+//Be effecient and accurate when typing code
+//Call a method to make something to happen. method such as Println(),
+//it then will display text on the screen.
+public class HelloWorld {//The name of the class can be choosen by me but the main class does not change.
+    //This class can have many methods but it must always have/containn main method otherwise it wont work.
+    //The class is public because it must be available to java environment.
+    //Programmers must write the code by creating the class and the main method. 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { // we have the main method, it is stored in the class
+        System.out.println("Hello World"); //println is the method.
+//Methods are stored in class
+//class is the pre-designed code that is available to programmers
+//~~This code is already there or created or  just that it is then made to be available for programmers without the programmers creating the code from scratch.
+//FUnny how the class is already created by Java programmers for PROGRAMMERS.
+//The classes we have are Math class, Scanner class and other classes that  i am soon to know about~LOL
+//Math class~~have or contain methods such as round(), SQRT() , floor() and ceiling()
+//Scanner class containg methods that can be called to obtain value from the keyboard.
+//Scanner k = new Scanner.next()
+        System.out.println("My name is Juliet");
+       //Syste.out is the container of print()
+       //()-THIS ARE ARGUMENTS
+       
+       
+       
+       
+       //System.out. println  is the method to display the output actually its commands the computer to take action
+    }
+    
+}
